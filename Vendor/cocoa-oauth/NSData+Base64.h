9a9a967981f41265c4c4a04e1f25cf50a528e75c
Johannes Stühler <johannes@getamen.com>
2013-02-15 13:54:19 +0100

    Fixed naming clash with static libs

--- Vendor/cocoa-oauth/NSData+Base64.h
+++ Vendor/cocoa-oauth/NSData+Base64.h
@@ -23,12 +23,12 @@
 
 #import <Foundation/Foundation.h>
 
-void *NewBase64Decode(
+void *NewBase64Decode2(
 	const char *inputBuffer,
 	size_t length,
 	size_t *outputLength);
 
-char *NewBase64Encode(
+char *NewBase64Encode2(
 	const void *inputBuffer,
 	size_t length,
 	bool separateLines,
